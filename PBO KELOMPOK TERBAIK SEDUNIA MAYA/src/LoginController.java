3d6132b6a3b8143866e28d73b9292f57d5f9d2fc
gitgudprint <152303838+gitgudprint@users.noreply.github.com>
2024-05-08 22:16:07 +0700

    Add files via upload

---
+++ PBO KELOMPOK TERBAIK SEDUNIA MAYA/src/LoginController.java
@@ -0,0 +1,37 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+public class LoginController {
+
+    @FXML
+    private Label LLogin;
+
+    @FXML
+    private Button LogButton;
+
+    @FXML
+    private Label Lversion;
+
+    @FXML
+    private Button SignupButton;
+
+    @FXML
+    private PasswordField tfpassword;
+
+    @FXML
+    private PasswordField tfusername;
+
+    @FXML
+    void LoginButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tosignup(ActionEvent event) {
+
+    }
+
+}
